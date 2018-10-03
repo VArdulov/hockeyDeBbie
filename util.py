045911b762f3a196d38989483e6d31c487cde586
Victor Ardulov <victor@ardulov.com>
2018-10-02 19:30:34 -0700

    initial commit

---
+++ util.py
@@ -0,0 +1,203 @@
+
+
+import spacy
+nlp = spacy.load('en')
+
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+from os.path import join
+from pandas import read_html
+
+
+cap_friendly_base_url = 'http://capfriendly.com/player/'
+
+all_categories = {'position': '/html/body/div[7]/div/div/div[1]/h6[2]/text()',
+                  'birthday':'/html/body/div[7]/div/div/div[3]/div[1]/text()',
+                  'birthplace':'/html/body/div[7]/div/div/div[3]/div[2]/text()',
+                  'height':'/html/body/div[7]/div/div/div[3]/div[3]/text()',
+                  'weight':'/html/body/div[7]/div/div/div[3]/div[4]/text()',
+                  'age':'/html/body/div[7]/div/div/div[4]/div[2]/text()',
+                  'draft_year':'//*[@id="pld_c3"]/div[1]/text()',
+                  'draft_team': '//*[@id="pld_c3"]/div[4]/text()',
+                  'draft_overall':'//*[@id="pld_c3"]/div[2]/text()'}
+
+contract_cateogries = ['CLAUSE', 'CAP HIT', 'AAV', 'P. BONUSES', 'S. BONUSES', 'BASE SALARY', 'TOTAL SALARY']
+
+skater_categories = ['TEAM', 'G', 'A', 'TP', 'GP', 'PIM']
+
+goalie_categories = ['TEAM', 'GP','GAA', 'SVS%']
+
+pnf_string = 'Player not found'
+
+practice_strings = ['Where does Erik Karlsson play?',
+                    'Where did John Tavares play in 2013?',
+                    'What was Drew Doughty\'s cap hit in 2014?',
+                    'How many goals did Joe Pavelski score in 2016?',
+                    'Where was Connor McDavid born?',
+                    'How tall is Ryan Getzlaf?',
+                    'What team drafted Roberto Luongo?',
+                    'Jesus Christ']
+
+
+html_parser = etree.HTMLParser()
+
+def parse_user_input(user_input=''):
+    understanding = {}
+    uil = user_input.lower()
+    if 'fight on' in uil:
+        understanding = {'intent':'easter egg'}
+        return understanding
+
+    if 'jesus christ' in uil:
+        understanding = {'intent':'jason bourne'}
+        return understanding
+
+
+    parsed_input = nlp(user_input)
+    entities = parsed_input.ents
+    ent_labels = [e.label_ for e in entities]
+    understanding['players'] = [entities[i].text for i, l in enumerate(ent_labels) if l == 'PERSON']
+    understanding['seasons'] = [entities[i].text for i, l in enumerate(ent_labels) if l == 'DATE' or l == 'CARDINAL']
+    understanding['teams'] = [entities[i].text for i, l in enumerate(ent_labels) if l == 'ORG']
+    
+    understanding['categories'] = []
+    if 'born' in uil: #really unique query
+        if 'where' in uil:
+            understanding['categories'].append('birthplace')
+        if 'when' in uil:
+            understanding['categories'].append('birthday')
+    
+    elif 'drafted' in uil: # draft specific query
+        understanding['draft'] = True
+
+        if 'who' in uil or 'team' in uil:
+            understanding['categories'].append('draft_team')
+
+        if 'when' in uil or 'year' in uil:
+            understanding['categories'].append('draft_year')
+
+        if 'where' in uil or 'position' in uil or 'overall' in uil:
+            understanding['categories'].append('draft_overall')
+
+    elif 'position' in uil:
+        understanding['categories'].append('position')
+
+    elif 'was' in uil or 'did' in uil:  # past tense query
+        understanding['past_tense'] = True
+
+
+    if 'old' in uil or 'age' in 'uil':
+        understanding['categories'].append('age')
+
+    if 'gaa' in uil or 'goals against' in uil:
+        understanding['categories'].append('GAA')
+    elif 'goals' in uil:
+        understanding['categories'].append('G')
+
+    if 'save' in uil or '%' in uil:
+        understanding['categories'].append('SVS%')
+
+    if 'team' in uil:
+        understanding['categories'].append('TEAM')
+
+    if 'assists' in uil or 'apples' in uil:
+        understanding['categories'].append('A')
+
+    if 'penalities' in uil or 'pim' in uil:
+        understanding['categories'].append('PIM')
+
+    if 'games played' in uil or 'gp' in uil:
+        understanding['categories'].append('GP')
+
+    if 'points' in uil in uil:
+        understanding['categories'].append('TP')
+
+    for c in contract_cateogries:
+        if c.lower() in uil:
+            understanding['categories'].append(c)
+
+    if 'performance bonus' in uil:
+        understanding['categories'].append('P. BONUSES')
+
+    if 'signing bonus' in uil:
+        understanding['categories'].append('S. BONUSES')
+
+    return understanding
+
+
+def get_stats(player_name, categories, season, playoff=False):
+    print(playoff)
+    name_to_url = '-'.join(player_name.lower().split())
+    get_url = join(cap_friendly_base_url, name_to_url)
+    capfriendly_request = requests.get(get_url)
+
+    output = {'request_status':(capfriendly_request.status_code == 200),
+              'player_found':False}
+
+    if capfriendly_request.status_code == 200: #Successfully exectued GET
+        html_doc = etree.fromstring(capfriendly_request.text, html_parser)
+        html_soup = BeautifulSoup(capfriendly_request.text, 'lxml')
+        if html_soup.body.h1.text != pnf_string:
+            output['player_found'] = True
+            output['position'] = html_doc.xpath(all_categories['position'])[0]
+            tables = html_soup.body.findChildren('table')
+            last_table_idx = len(tables) - 1
+            current_contract_table = read_html(str(tables[0]), header=0, index_col=0)[0]
+            career_stats_table = read_html(str(tables[last_table_idx]), header=0, index_col=0)[0]
+
+            for c in categories:
+                if c in all_categories:
+                    output[c] = html_doc.xpath(all_categories[c])[0]
+                elif c in contract_cateogries:
+                    # print('accessing contract categories')
+                    output[c] = current_contract_table.get(c)
+                    # print('output', output[c])
+                    if output[c] is not None:
+                        # print('made it here')
+                        output[c] = output[c].get(season[0])
+                elif c in skater_categories or c in goalie_categories:
+                    c_name = c if not playoff else (c + '.1')
+                    output[c] = career_stats_table.get(c_name)
+
+                    # print('accessing states categories')
+                    # print('output', output[c])
+
+                    if output[c] is not None:
+                        output[c] = output[c].get(season[1])
+
+                else:
+                    output[c] = 'invalid'
+
+    return output
+
+
+if __name__ == '__main__':
+    # Test script
+    print('Beginning util test')
+    print("importing 'random'")
+    from numpy.random import choice, randint
+    print('constructing player - stat splits')
+    player_names = ['Erik Karlsson', 'Gritty', 'Craig Anderson',
+                    'John Tavares', 'Brock Boeser', 'Evgeni Nabokov']
+
+    print('generating requests')
+    categories_per_player = [randint(1, 5) for name in player_names]
+
+    for i in range(len(player_names)):
+        choices = choice(list(all_categories.keys()) +\
+                         contract_cateogries +\
+                         skater_categories + \
+                         goalie_categories, categories_per_player[i], replace=False)
+        print(player_names[i])
+        print('\t', choices)
+        out = get_stats(player_names[i], choices, ['2017-18', '2017-2018'], playoff=False)
+        print('\t', out)
+
+    print('data access tests completed')
+    print('\n\n')
+    print('starting nlp tests')
+
+    for s in practice_strings:
+        print(s)
+        print('\t', parse_user_input(s))
